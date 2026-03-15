b86116abe1edefe59088d0af94bd0e9136689e7a
Dushime Aline Denise <alinedushdenyse@gmail.com>
2026-03-15 23:39:05 +0200

    update of loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,7 @@
 import { Box, CircularProgress } from '@mui/material'
 
 const Loader = () => (
-  <Box
-    display="flex"
-    justifyContent="center"
-    alignItems="center"
-    height="100vh"
-  >
+  <Box display="flex" justifyContent="center" alignItems="center" height="60vh">
     <CircularProgress sx={{ color: '#ff0000' }} />
   </Box>
 )
